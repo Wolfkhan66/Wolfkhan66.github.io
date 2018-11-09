5f5086a64cc2d6f4437d5b9d8743f74d4af69120
Cai Lehwald <cailehwald@googlemail.com>
2018-11-09 16:43:53 +0000

    Implement partials functionality using jquery
    Refactor the header and navigation from the index page into partial pages that are loaded by jquery to avoid repeating the html in every page.
    Refactor the projects list out of the index into its own partial and add the pre-srollable class.
    Nest the jquery fade in function to call each fade in after the parent is complete.

---
+++ js/utility.js
@@ -0,0 +1,7 @@
+(function($, window, document, undefined) {
+  $(function() {
+    $("#header").load("partials/header.html");
+    $("#navigation").load("partials/navigation.html");
+    $("#projectList").load("partials/projects.html");
+  });
+})(jQuery, window, document);
